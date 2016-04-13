9ae5b2ee01a36c915b05274b5da39b14c9055994
Diogo Leitão <diogo.n.leitao@gmail.com>
2016-04-13 19:12:19 +0100

    Referencing global values not working as expected. Might need to change the approach to a more modular one.

--- LinkCrawler.py
+++ LinkCrawler.py
@@ -16,18 +16,20 @@ class LinkCrawler:
         # modification, instead of passing it as an argument and handling it
         # as a copy of the original queue.
         global links_queue
-
-        url_index = links_queue.index(self.url_seed)
-        links_queue.remove(url_index)
+        global parsed_links_queue
+        global list_changed
 
         page = requests.get(self.url_seed, cookies=self.cookies).text
         anchors = BeautifulSoup(page, 'html.parser').find_all('a')
         for anchor in anchors:
             href = anchor.get("href")
             try:
-                if href.startswith("/"):  # ONLY SAVING LOCAL HREFS
-                    if href not in links_queue:
+                if href.startswith("/"):  # Only save local links
+                    if href not in parsed_links_queue:
+                        parsed_links_queue.append(href)
                         links_queue.append(href)
+                        list_changed = True
+
             except TypeError:
                 # Some href attributes are blank or aren't of type 'string',
                 # which can't be coerced; so, we just ignore the errors
