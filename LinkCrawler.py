bdd2e49c080be4661b4c16650c0266bb2b8deef5
Diogo Leitão <diogo.n.leitao@gmail.com>
2016-04-15 18:32:33 +0100

    LinkCrawler.py HTTP request refactored; href condition fixed

--- LinkCrawler.py
+++ LinkCrawler.py
@@ -8,27 +8,23 @@ class LinkCrawler:
     cookies = {}
 
     def __init__(self, url_seed, cookies):
-        self.url_seed = url_seed
+        self.url_seed = globalvars.base_url + url_seed
         self.cookies = cookies
-        return
 
     def crawl(self):
-        # By declaring this variable as global, it enables direct access and
-        # modification, instead of passing it as an argument and handling it
-        # as a copy of the original queue.
-
-        page = requests.get(self.url_seed, cookies=self.cookies).text
-        anchors = BeautifulSoup(page, 'html.parser').find_all('a')
+        request = requests.get(self.url_seed, cookies=self.cookies)
+        html_tree = request.text
+        anchors = BeautifulSoup(html_tree, 'html.parser').find_all('a')
         for anchor in anchors:
             href = anchor.get("href")
             try:
-                if href.startswith("/"):  # Only save same domain links
+                # Only save same domain links
+                if href.startswith(globalvars.cookies['contextPath']) or href.startswith(globalvars.base_url):
                     if href not in globalvars.parsed_links_queue:
                         globalvars.parsed_links_queue.append(href)
                         globalvars.links_queue.append(href)
-                        globalvars.list_changed = True
 
-            except TypeError:
+            except AttributeError:
                 # Some href attributes are blank or aren't of type 'string',
                 # which can't be coerced; so, we just ignore the errors
                 continue
