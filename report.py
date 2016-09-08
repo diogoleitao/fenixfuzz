e6cd4fab724d6df7fca7419561f056b0041e6a02
Diogo Leitão <diogo.n.leitao@gmail.com>
2016-09-08 01:17:05 +0100

    HTML report file generation done; added custom templates to facilitate the filling in part

--- report.py
+++ report.py
@@ -4,8 +4,6 @@
 
 import json
 
-import json2html
-
 import globalvars
 
 
@@ -48,28 +46,15 @@ def generate_html_report():
         Generates HTML report file from JSON file
     """
 
-    generate_json_report()
+    # generate_json_report()
 
-    # with open("output/report_test.html", "w") as report_file:
-    json_data = None
+    templates = load_templates()
+
     with open("output/report.json", "r") as json_file:
         json_data = json.loads(json_file.read())
-    for data in json_data:
-        url = data['url']
 
-        forms = data['forms']
-        for form in forms:
-            fid = form['id']
-            method = form['method']
-            action = form['action']
-            error_code = form['error']['code']
-            error_message = form['error']['message']
-
-            fields = form['fields']
-            for field in fields:
-                name = field['name']
-                ftype = field['type']
-                value = field['value']
+    with open("output/report_test.html", "w") as report_file:
+        report_file.write(templates[0].replace("{pages}", fill_page_templates(json_data, templates)))
 
 
 def generate_json_report():
@@ -79,3 +64,57 @@ def generate_json_report():
 
     with open("output/report.json", "w") as report_file:
         json.dump(globalvars.ERRORS, report_file)
+
+
+def load_templates():
+    """
+        Reads the contents of each HTML template file and returns them as a tuple
+    """
+
+    with open("report-templates/report-main.html") as template:
+        main = template.read()
+
+    with open("report-templates/report-page.html") as template:
+        page = template.read()
+
+    with open("report-templates/report-form.html") as template:
+        form = template.read()
+
+    with open("report-templates/report-field.html") as template:
+        field = template.read()
+
+    return main, page, form, field
+
+
+def fill_page_templates(pages, templates):
+    """
+        Substitute the URL and list of forms for all the pages
+    """
+
+    final_template = ""
+    for page in pages:
+        final_template += templates[1].replace("{url}", page['url']).replace("{forms}", fill_form_templates(page['forms'], templates))
+    return final_template
+
+
+def fill_form_templates(forms, templates):
+    """
+        Substitute the id, method, action, code message and list of fields for
+        all the forms
+    """
+
+    final_template = ""
+    for form in forms:
+        final_template += templates[2].replace("{id}", form['id']).replace("{method}", form['method']).replace("{action}", form['action']).replace("{code}", form['error']['code']).replace("{message}", form['error']['message']).replace("{fields}", fill_field_templates(form['fields'], templates))
+    return final_template
+
+
+def fill_field_templates(fields, templates):
+    """
+        Substitute the name, type and value for all the fields
+    """
+
+    final_template = ""
+    for field in fields:
+        final_template += templates[3].replace("{name}", field['name']).replace("{type}", field['type']).replace("{value}", field['value'])
+    return final_template
